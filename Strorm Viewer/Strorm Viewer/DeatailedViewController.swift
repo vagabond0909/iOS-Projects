275cf1634e4dd864910f0b13bbf7ea7e245ca0d6
Mohit Katkar <75497581+vagabond0909@users.noreply.github.com>
2022-07-25 10:56:48 +0530

    Add files via upload

---
+++ Strorm Viewer/Strorm Viewer/DeatailedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DeatailedViewController.swift
+//  Strorm Viewer
+//
+//  Created by Mohit Katkar on 09/02/22.
+//
+
+import UIKit
+
+class DeatailedViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
